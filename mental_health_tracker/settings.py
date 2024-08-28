903d03824031986a8ac5b2f20a454691ba2f6e91
dhaniatiara <dhania.t.herdiani@gmail.com>
2024-08-28 14:09:07 +0700

    add url deployment

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7ypbrk9l$@z*46s7xxnxmn+maua0ct(p&5ulmmof@3iaygdy#7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "dhania-tiaraputri-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition
